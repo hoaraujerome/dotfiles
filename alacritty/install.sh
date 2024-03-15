3df0adc430e3f6ef78017030ee7527283e5e412e
Jerome Hoarau <jhoarau@petalmd.com>
2024-03-15 22:20:21 +0400

    feat: bootstrap alacritty installation

---
+++ alacritty/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+brew install --cask alacritty
+
+brew tap homebrew/cask-fonts
+brew install --cask font-ubuntu-mono-nerd-font
+
+cp ~/dotfiles/alacritty/alacritty.toml ~/.alacritty.toml    
