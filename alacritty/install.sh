35c7f56db0f9d6da082e9ddd977fd3aff069539a
Jerome Hoarau <jhoarau@petalmd.com>
2024-03-22 13:59:00 -0400

    feat: enable alacritty smooting on mac

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -8,3 +8,8 @@ brew tap homebrew/cask-fonts
 brew install --cask font-ubuntu-mono-nerd-font
 
 cp ~/dotfiles/alacritty/.alacritty.toml ~/
+
+# https://pezcoder.medium.com/how-i-migrated-from-iterm-to-alacritty-c50a04705f95
+# Enable smoothing on mac
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+defaults -currentHost write -globalDomain AppleFontSmoothing -int 2
