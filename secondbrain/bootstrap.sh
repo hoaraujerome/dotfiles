bd774febfe6325f5fc35399253f5521044d45f0c
Jerome Hoarau <jhoarau@petalmd.com>
2024-03-25 10:56:12 -0400

    feat: bootstrap secondbrain

---
+++ secondbrain/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SECOND_BRAIN_PATH="$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/secondbrain"
+
+mkdir -p "$SECOND_BRAIN_PATH"
+
+git clone git@github.com:hoaraujerome/coding.git "$SECOND_BRAIN_PATH/coding"
