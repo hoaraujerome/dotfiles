be77a4dbcfb1518148075bf4950fc220cf610baf
Jay <jayrome82@gmail.com>
2024-03-29 14:26:23 -0400

    fix: bad starship configuration path

--- bash/bootstrap.sh
+++ bash/bootstrap.sh
@@ -6,6 +6,6 @@ brew install starship
 
 cp ~/dotfiles/bash/.bash_profile ~/
 cp ~/dotfiles/bash/.bashrc ~/
-cp ~/dotfiles/bash/starship.toml ~/
+cp ~/dotfiles/bash/starship.toml ~/.config/
 
 chsh -s /bin/bash $(whoami)
