b6a4300879ced87fca1610b957add70d13f9f9df
Jay <jayrome82@gmail.com>
2025-01-18 15:15:12 -0500

    feat: add devbox

---
+++ devbox/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+# https://github.com/jetify-com/devbox/releases
+export DEVBOX_USE_VERSION=0.13.7
+
+# https://www.jetify.com/docs/devbox/installing_devbox/?install-method=macos#install-devbox
+curl -fsSL https://get.jetify.com/devbox | bash
--- nix/install_nix.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# https://releases.nixos.org/?prefix=nix/
-export VERSION=2.21.0
-
-# https://nixos.org/manual/nix/stable/installation/installing-binary
-curl -L https://releases.nixos.org/nix/nix-$VERSION/install | sh -s -- --daemon
