92f2357b54416fcb55cd37b479fbcb0acbc8da49
Jay <jayrome82@gmail.com>
2025-06-24 21:39:50 -0400

    fix: devbox installation

--- devbox/bootstrap.sh
+++ devbox/bootstrap.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-# https://github.com/jetify-com/devbox/releases
-export DEVBOX_USE_VERSION=0.13.7
+# NIX
+# Install nix beforehand and standalone.
+# For some reasons, the devbox embedded nix installation did not work.
+export VERSION=2.28.3
+curl -L https://releases.nixos.org/nix/nix-$VERSION/install | sh
 
+# DEVBOX
+# https://github.com/jetify-com/devbox/releases
+export DEVBOX_USE_VERSION=0.14.2
 # https://www.jetify.com/docs/devbox/installing_devbox/?install-method=macos#install-devbox
 curl -fsSL https://get.jetify.com/devbox | bash
