777c37e0ef17786dc6d0fee53a81e8df1f4fa81b
Jerome Hoarau <jhoarau@petalmd.com>
2026-01-02 16:17:24 -0500

    feat: zed

--- zed/bootstrap.sh
+++ zed/bootstrap.sh
@@ -12,6 +12,7 @@ cp ~/dotfiles/zed/*.json ~/.config/zed/
 # Manually install extensions:
 # - opencode
 # - terraform
+# - Terraform Context Server
 
 # Sign in to get code predictions
 # https://zed.dev/docs/ai/edit-prediction#edit-prediction
